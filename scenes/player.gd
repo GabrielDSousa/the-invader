23de3da6eca1b1f3168f1540ac3471abdf059a25
GabrielDSousa <gabrielramos.email@gmail.com>
2026-07-19 10:54:51 -0300

    Skybom, map v1.0, map and characther integration

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,22 +5,27 @@ extends RigidBody3D
 @onready var ufo_beam_burst: MeshInstance3D = $"beam/ufo-beam-burst"
 @onready var ufo_mesh: MeshInstance3D = $ufo
 @onready var player_collision: CollisionShape3D = $collision
+@onready var beam_collision: CollisionShape3D = $beam/collision
 @onready var beam: Area3D = $beam
-@onready var level_value: Label = $view/HUD/XP/value
+@onready var xp_value: Label = $view/HUD/XP/value
+@onready var level_value: Label = $view/HUD/level/value
+@onready var camera: Camera3D = $view/Camera
 
 
 const VELOCITY: float = 10.0
 const MAX_LEVEL: float = 6.0
 const INITIAL_LEVEL: float = 1.0
+const MAX_LAYER: int = 4
+const XP_WEIGHT: int = 20
 
-
+var current_layer = MAX_LAYER
 var level: float = INITIAL_LEVEL
 var bursting: bool = false
 
 func _ready() -> void:
 	position.y = INITIAL_LEVEL
 	set_scale_from_multiplier(INITIAL_LEVEL)
-	beam.connect("body_shape_entered", drag_object)
+	beam.connect("body_entered", drag_object)
 
 
 func _physics_process(_delta: float) -> void:
@@ -43,11 +48,13 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_accept"):
 		if bursting:
 			stop_burst()
+			current_layer = MAX_LAYER
 
 func start_burst() -> void:
 	bursting = true
 	animation_player.stop()
 	animation_player.play("de_active")
+	beam_collision.set_deferred("disabled", false)
 	await animation_player.animation_finished
 	burst()
 
@@ -60,49 +67,56 @@ func stop_burst() -> void:
 	animation_player.stop()
 	animation_player.play_backwards("de_active")
 	await animation_player.animation_finished
+	beam_collision.set_deferred("disabled", true)
 	ufo_beam.visible = false
 	ufo_beam_burst.visible = false
 
-func drag_object(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
-	if body is GridMap:
-		var map_position: Vector3i = body.local_to_map(beam.global_position)
-		map_position = Vector3i(map_position.x, 1, map_position.z)
-		var item_id: int = body.get_cell_item(map_position)
-		if item_id != -1:
-			var mesh: Mesh = body.mesh_library.get_item_mesh(item_id)
-			var mesh_instance := MeshInstance3D.new()
-			mesh_instance.position = body.map_to_local(map_position)
-			mesh_instance.mesh = mesh
-			body.set_cell_item(map_position, body.INVALID_CELL_ITEM)
-			body.add_child(mesh_instance)
+func drag_object(body: Node3D) -> void:
+	while current_layer > 0:
+		if body is GridMap:
+			var map_position: Vector3i = body.local_to_map(beam.global_position)
+			map_position = Vector3i(map_position.x, current_layer, map_position.z)
+			var item_id: int = body.get_cell_item(map_position)
+			if item_id != -1:
+				var mesh: Mesh = body.mesh_library.get_item_mesh(item_id)
+				var mesh_instance := MeshInstance3D.new()
+				mesh_instance.position = body.map_to_local(map_position)
+				mesh_instance.mesh = mesh
+				body.set_cell_item(map_position, body.INVALID_CELL_ITEM)
+				body.add_child(mesh_instance)
 
-			var xp = mesh_instance.get_aabb().get_volume() * 500
+				var xp = mesh_instance.get_aabb().get_volume()
 
-			var time_left: float = xp/100
-			while time_left > 0:
-				if not bursting:
-					# Revert drag
-					mesh_instance.queue_free()
-					body.set_cell_item(map_position, item_id)
-					return
-				# Scale Y and and fade out
-				mesh_instance.scale.y += 0.01
-				time_left -= 0.1
-				await get_tree().create_timer(0.1).timeout
-			mesh_instance.queue_free()
-			add_xp(xp)
+				var time_left: float = xp * 100
+				while time_left > 0:
+					if not bursting:
+						# Revert drag
+						mesh_instance.queue_free()
+						body.set_cell_item(map_position, item_id)
+						current_layer = MAX_LAYER
+						return
+					# Scale Y and and fade out
+					mesh_instance.scale.y += 0.1
+					time_left -= 1
+					await get_tree().create_timer(0.5).timeout
+				mesh_instance.queue_free()
+				add_xp(xp)
+		current_layer -= 1
 
 
 func set_scale_from_multiplier(multiplier: float) -> void:
 	beam.scale = beam.scale.lerp(beam.scale * multiplier, 1.0)
 	ufo_mesh.scale = ufo_mesh.scale.lerp(ufo_mesh.scale * multiplier, 1.0)
 	player_collision.scale = player_collision.scale.lerp(player_collision.scale * multiplier, 1.0)
+	player_collision.scale = player_collision.scale.lerp(player_collision.scale * multiplier, 1.0)
+	position = position.lerp(Vector3(position.x, ufo_mesh.scale.y, position.z), 1.0)
 
 func add_xp(value: float) -> void:
-	level_value.text = var_to_str(ceil(value))
-	if value > 100 * level:
+	xp_value.text = var_to_str(ceil(float(xp_value.text) + (value) + XP_WEIGHT))
+	if (float(xp_value.text) > (100 * level)):
 		level_up()
 
 func level_up() -> void:
 	level += 1
+	level_value.text = var_to_str(level)
 	set_scale_from_multiplier(level)
